34f7f881fe3b605fe08ecde4c33924bbcffe3fec
vivekanandpv-mac <vivekanandpv@outlook.com>
2019-08-22 14:14:21 +0530

    onchanges implemented

--- src/app/playground/playground.component.ts
+++ src/app/playground/playground.component.ts
@@ -17,14 +17,23 @@ import {
   templateUrl: './playground.component.html',
   styleUrls: ['./playground.component.css']
 })
-export class PlaygroundComponent implements OnInit {
+export class PlaygroundComponent implements OnInit, OnChanges {
   state = 'Something';
+  @Input() prop;
 
   constructor() {}
 
   ngOnInit() {}
 
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Changes!', changes);
+  }
+
   handler(args) {
     console.log(args);
+  }
+
+  clickHandler() {
+    console.log('clicked');
   }
 }
