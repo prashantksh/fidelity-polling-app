2ee6996e6447d7e47df408ce066ed9a2a3d00662
Vivekanand P V <vivekanandpv@outlook.com>
2019-08-22 23:01:09 +0530

    Forms starter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { PollContainerComponent } from './poll-container/poll-container.component';
 import { PollItemComponent } from './poll-item/poll-item.component';
 import { PlaygroundComponent } from './playground/playground.component';
+import { TformComponent } from './tform/tform.component';
+import { RformComponent } from './rform/rform.component';
 
 @NgModule({
   declarations: [
@@ -13,12 +16,12 @@ import { PlaygroundComponent } from './playground/playground.component';
     NavComponent,
     PollContainerComponent,
     PollItemComponent,
-    PlaygroundComponent
+    PlaygroundComponent,
+    TformComponent,
+    RformComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/rform/rform.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rform',
+  templateUrl: './rform.component.html',
+  styleUrls: ['./rform.component.css']
+})
+export class RformComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
