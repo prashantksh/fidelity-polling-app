2ad6dbd3ef8bb540e7f5f8c67d9464ea5f9d8c38
vivekanandpv-mac <vivekanandpv@outlook.com>
2019-08-23 12:58:47 +0530

    Async validator demo

--- src/app/rform/rform.component.ts
+++ src/app/rform/rform.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  AbstractControl
+} from '@angular/forms';
 
 @Component({
   selector: 'app-rform',
@@ -8,14 +13,16 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class RformComponent implements OnInit {
   pollForm: FormGroup;
+  validTitle = 'My Title';
 
   constructor() {
     this.pollForm = new FormGroup({
       id: new FormControl(0, Validators.required),
-      title: new FormControl('', [
-        Validators.required,
-        Validators.minLength(5)
-      ]),
+      title: new FormControl(
+        '',
+        [Validators.required, Validators.minLength(5)],
+        this.validateTitle.bind(this)
+      ),
       description: new FormControl(),
       group: new FormControl('Development'),
       imageUrl: new FormControl()
@@ -23,4 +30,18 @@ export class RformComponent implements OnInit {
   }
 
   ngOnInit() {}
+
+  validateTitle(ctrl: AbstractControl): Promise<any> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const val = ctrl.value;
+        if (val === this.validTitle) {
+          // Success case
+          resolve(null);
+        } else {
+          resolve({ title_error: true });
+        }
+      }, 2000);
+    });
+  }
 }
