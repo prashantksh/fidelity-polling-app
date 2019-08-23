3889337ba9d4ac73c26524af3966b4f8824aa205
vivekanandpv-mac <vivekanandpv@outlook.com>
2019-08-23 12:36:28 +0530

    Reactive form basic

--- src/app/rform/rform.component.ts
+++ src/app/rform/rform.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-rform',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rform.component.css']
 })
 export class RformComponent implements OnInit {
+  pollForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.pollForm = new FormGroup({
+      id: new FormControl(0, Validators.required),
+      title: new FormControl('', [
+        Validators.required,
+        Validators.minLength(5)
+      ]),
+      description: new FormControl(),
+      group: new FormControl('Development'),
+      imageUrl: new FormControl()
+    });
   }
 
+  ngOnInit() {}
 }
